215fe6ba1ee79d66c9acf292dfbcbd6e47a71b5c
Arghya Basu <arghyabs@gmail.com>
2019-09-24 17:13:32 +0000

    Correcting Deployment

--- deployment.sh
+++ deployment.sh
@@ -35,6 +35,7 @@ docker tag ${IMAGE} gcr.io/${PROJECT_ID}/${IMAGE}:$CIRCLE_SHA1
 gcloud auth configure-docker
 gcloud docker -- push gcr.io/${PROJECT_ID}/${IMAGE}:$CIRCLE_SHA1
 #kubectl delete deployment ${DEPLOYMENT_NAME}
+kubectl delete pod ${IMAGE} --now
 kubectl create deployment ${DEPLOYMENT_NAME} --image=gcr.io/${PROJECT_ID}/${IMAGE}:$CIRCLE_SHA1
 kubectl get pods
 
