f40f0723db1664ea2b3ea1e1b22202d3a333904c
Arghya Basu <arghyabs@gmail.com>
2019-09-25 02:45:49 +0000

    Test

--- test/test.js
+++ test/test.js
@@ -1,11 +1,18 @@
- var assert = require('assert'),
-     http = require('http');
+// var assert = require('assert'),
+//     http = require('http');
 
- describe('/', function () {
-   it('should return 200', function (done) {
-     http.get('http://localhost:3000', function (res) {
-       assert.equal(200, res.statusCode);
-       done();
-     });
-   });
- });
+// describe('/', function () {
+//   it('should return 200', function (done) {
+//     http.get('http://localhost:3000', function (res) {
+//       assert.equal(200, res.statusCode);
+//       done();
+//     });
+//   });
+// });
+
+const assert = require('assert');
+const greetings = require('./hello.js');
+
+assert.strictEqual(greetings.sayHelloInEnglish(), 'HELLO', 'our "Hello" function works');
+assert.strictEqual(greetings.sayHelloInSpanish(),'Hola','our "Hola" function works');
+assert.strictEqual(greetings.sayHelloInFrench(),'Bonjour','our "Bonjour" function works');
