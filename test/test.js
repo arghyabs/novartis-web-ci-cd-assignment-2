650a840115598e45f2063804a099088e8a782c65
Arghya Basu <arghyabs@gmail.com>
2019-09-25 03:20:40 +0000

    Test

--- test/test.js
+++ test/test.js
@@ -13,11 +13,19 @@
 var  assert = require('assert');
 var greetings = require('../hello.js');
 
-describe('Calculator Tests', function() {
+describe('Hello Tests', function() {
 	it('our "Hello" function works', function (done) {
 		assert.equal('HELLO', greetings.sayHelloInEnglish());
 		done();
 	});
+	it('our "Hola" function works', function (done) {
+                assert.equal('Hola', greetings.sayHelloInSpanish());
+                done();
+        });
+	it('our "Bonjour" function works', function (done) {
+                assert.equal('Bonjour', greetings.sayHelloInFrench());
+                done();
+        });
 });
 
 //assert.strictEqual(greetings.sayHelloInEnglish(), 'HELLO', 'our "Hello" function works');
