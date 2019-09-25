8ed87526b40cd2d232b44a0e548a91adf322bdc0
Arghya Basu <arghyabs@gmail.com>
2019-09-25 02:48:24 +0000

    Test

--- test/test.js
+++ test/test.js
@@ -11,7 +11,7 @@
 // });
 
 const assert = require('assert');
-const greetings = require('./hello.js');
+const greetings = require('../hello.js');
 
 assert.strictEqual(greetings.sayHelloInEnglish(), 'HELLO', 'our "Hello" function works');
 assert.strictEqual(greetings.sayHelloInSpanish(),'Hola','our "Hola" function works');
