8e108d1cc0922318a9a337d8af1bcb63b51b5f0e
Arghya Basu <arghyabs@gmail.com>
2019-09-25 11:55:39 +0000

    Test

---
+++ hello.js
@@ -0,0 +1,12 @@
+module.exports = {
+	sayHelloInEnglish: function() {
+		return "HELLO";
+	},
+
+	sayHelloInSpanish: function() {
+		return "Hola";
+	},
+	sayHelloInFrench: function(){
+		return "Bonjour"
+	}
+};
