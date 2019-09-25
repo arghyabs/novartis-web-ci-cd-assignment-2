a13653876e1a26de323a8aef513a22fbebda4266
Arghya Basu <arghyabs@gmail.com>
2019-09-25 11:33:14 +0000

    Test

--- hello.test.js
+++
@@ -1,6 +0,0 @@
-const assert = require('assert');
-const greetings = require('./hello.js');
-
-assert.strictEqual(greetings.sayHelloInEnglish(), 'HELLO', 'our "Hello" function works');
-assert.strictEqual(greetings.sayHelloInSpanish(),'Hola','our "Hola" function works');
-assert.strictEqual(greetings.sayHelloInFrench(),'Bonjour','our "Bonjour" function works');
