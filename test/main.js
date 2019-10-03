c27e680918ea0fef312fb2e5a94f415002be3891
Arghya Basu <arghyabs@gmail.com>
2019-10-03 18:56:10 +0000

    Test

--- test/main.js
+++ test/main.js
@@ -1,4 +1,4 @@
-const siteUrl = 'http://${require(‘ip’).address()}:3000'
+const siteUrl = 'http://0.0.0.0:3000'
 
 module.exports = {
   'Checking application index page in Chrome': function (browser) {
