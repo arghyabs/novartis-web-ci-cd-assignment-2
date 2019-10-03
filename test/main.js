a0cefb33dcf962987e65598cbb237cca7b6b44cd
Arghya Basu <arghyabs@gmail.com>
2019-10-03 17:28:57 +0000

    Test

--- test/main.js
+++ test/main.js
@@ -1,7 +1,7 @@
 module.exports = {
   'Checking application index page in Chrome': function (browser) {
     browser
-      .url("http://localhost:3000/index.html")
+      .url("http://${require(‘ip’).address()}:3000/index.html")
       .pause(2000)
       .assert.titleContains('Novartis')
       .pause(2000)
