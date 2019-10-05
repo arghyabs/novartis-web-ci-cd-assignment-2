3b35acd19fadf96be4158c74227d6eb93cb0f87f
Arghya Basu <arghyabs@gmail.com>
2019-10-05 04:42:53 +0000

    Test

--- deployment.sh
+++ deployment.sh
@@ -8,10 +8,6 @@ echo $ACCOUNT_KEY_STAGING > service_key.txt
 
 base64 -i service_key.txt -d > ${HOME}/gcloud-service-key.json
 
-curl https://sdk.cloud.google.com | bash
-exec -l --y $SHELL
-gcloud init
-
 gcloud auth activate-service-account ${ACCOUNT_ID} --key-file ${HOME}/gcloud-service-key.json
 
 gcloud config set project $PROJECT_ID
